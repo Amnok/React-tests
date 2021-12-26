91c4de081f32890c03fa02b0a7a5e8897feaccea
Amnok <ankitsingh80741@gmail.com>
2021-12-26 23:32:40 +0530

    added basic

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Component1 from './hocs-test/component1';
 import Component2 from './hocs-test/component2';
 import withLoader from './hocs-test/withLoader';
 import UseReducerTest from './hooks/useReducerTest';
+import UseState from './hooks/useState';
 import Multi from './state-tests/multi';
 import ClassTick from './tick-tick/classTick';
 import FuntionalTick from './tick-tick/funtionalTick';
@@ -16,7 +17,7 @@ import FuntionalTick from './tick-tick/funtionalTick';
 function App() {
   return (
     <div className='container'>
-      <AllAboutFormik/>
+      <UseState/>
     </div>
   );
 }
