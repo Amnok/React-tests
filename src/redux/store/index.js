2bcfa72478727b6819bb556e0b0ca86109e1cd3a
Amnok <ankitsingh80741@gmail.com>
2022-01-10 19:50:25 +0530

    devtools started

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,13 +1,21 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+// import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import { fromPairs } from 'lodash';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit';
 import notesReducer from './notes';
 import moviesReducer from './movies';
-const store = createStore(
-  combineReducers({notesReducer, moviesReducer}),
-    composeWithDevTools(
-      applyMiddleware(thunk)
-      // other store enhancers if any
-    )
-  );
-export default store;
+
+// const store = createStore(
+//   combineReducers({notesReducer, moviesReducer}),
+//     composeWithDevTools(
+//       applyMiddleware(thunk)
+//       // other store enhancers if any
+//     )
+//   );
+// export default store;
+
+const reducer = {
+  notesReducer, moviesReducer
+}
+export default configureStore({reducer});
---
+++ src/redux/store/movies-devtools.js
@@ -0,0 +1,92 @@
+import axios from 'axios';
+import _ from 'lodash';
+import { createAction } from '@reduxjs/toolkit';
+// actions
+export const ADD_MOVIE = 'ADD_MOVIE';
+export const REMOVE_MOVIE = 'REMOVE_MOVIE';
+export const UPDATE_MOVIE = 'UPDATE_MOVIE';
+export const GET_MOVIES = 'GET_MOVIES';
+export const LOADING = 'LOADING';
+
+// export const addMovie = createAction('addMovie');
+// action creators
+
+async function fetchMovies() {
+  // return new Promise(async (resolve, reject) => {
+  //   const { data } = await axios.get('http://localhost:3006/movies');
+  //   setTimeout(() => {
+  //     resolve(data);
+  //   }, 0);
+  // });
+      const { data } = await axios.get('http://localhost:3006/movies');
+      return data;
+}
+export const getMovies = () => {
+  return async (dispatch) => {
+    dispatch({ type: LOADING, payload: [] });
+    const data = await fetchMovies();
+    dispatch({ type: GET_MOVIES, payload: data });
+  };
+};
+
+export const addMovie = (movie) => {
+  return async (dispatch, getState) => {
+    const state = getState();
+    const movies = _.get(state, 'moviesReducer.movies');
+    dispatch({ type: LOADING, payload: [] });
+    const _data = {
+      id: (movies.length + _.random(5000)).toString(),
+      ...movie
+    };
+    const { data } = await axios.post('http://localhost:3006/movies', _data);
+    dispatch({ type: ADD_MOVIE, payload: data });
+  };
+};
+
+export const removeMovie = (movie) => {
+  return async (dispatch) => {
+    dispatch({ type: LOADING, payload: [] });
+    const { data } = await axios.delete(
+      `http://localhost:3006/movies/${movie.id}`
+    );
+    console.log(data);
+    dispatch({ type: REMOVE_MOVIE, payload: movie });
+  };
+};
+
+export const editMovie = (movie) => {
+  return async (dispatch) => {
+    dispatch({ type: LOADING, payload: [] });
+    const { data } = await axios.put(`http://localhost:3006/movies/${movie.id}`, movie);
+    console.log(data);
+    dispatch({ type: UPDATE_MOVIE, payload: movie });
+  };
+};
+
+// reducer
+
+const initialState = {
+  movies: [],
+  error: {},
+  isLoading: false,
+};
+
+export default function moviesReducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case ADD_MOVIE:
+      return { ...state, movies: [...state.movies, payload], isLoading: false };
+    case REMOVE_MOVIE:
+      const updatedMovies = state.movies.filter((movie) => movie.id !== payload.id);
+      return { ...state, movies: updatedMovies, isLoading: false };
+    case UPDATE_MOVIE:
+      const tempmovies = state.movies.filter((movie) => movie.id !== payload.id);
+      return { ...state, movies: [...tempmovies, payload], isLoading: false };
+    case GET_MOVIES:
+      return { ...state, movies: payload, isLoading: false };
+    case LOADING:
+      return { ...state, isLoading: true };
+    default:
+      return state;
+  }
+}
